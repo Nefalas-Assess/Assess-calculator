85c5e71335b1df951b51aa75ce14db09228ad244
Thomas Point <thomas@makeit-studio.com>
2025-01-12 19:49:20 +0100

    start it

---
+++ src/renderer/src/components/it.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useContext, useState } from 'react'
+import { AppContext } from '@renderer/providers/AppProvider'
+import IncapaciteTemporaireForm from '@renderer/form/incapacite_temporaire'
+
+const IT = () => {
+  const { data, setData } = useContext(AppContext)
+
+  const saveData = useCallback(
+    (values) => {
+      console.log(values)
+      //   setData({ incapacite_temp: values })
+    },
+    [setData]
+  )
+
+  return (
+    <div id="content">
+      <div id="main">
+        <IncapaciteTemporaireForm onSubmit={saveData} initialValues={data?.incapacite_temp} />
+      </div>
+    </div>
+  )
+}
+
+export default IT
