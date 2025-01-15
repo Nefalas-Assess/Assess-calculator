8becc6bcedef560a335f35c6b6e605c519acce60
Nefalas-Assess <vdbjulien9@gmail.com>
2025-01-15 21:11:47 +0100

    Dernières feuilles de data + frais capitalisé mais sans mise en forme.

--- src/renderer/src/components/frais_cap.tsx
+++ src/renderer/src/components/frais_cap.tsx
@@ -1,16 +1,68 @@
-import React, { useCallback, useContext, useState } from 'react'
-import { AppContext } from '@renderer/providers/AppProvider'
-import FraisForm from '@renderer/form/frais_form'
+import React, { useState } from 'react'
 
 const Frais_cap = () => {
-  const { data, setData } = useContext(AppContext)
 
 
 
+    
   return (
     <div id="content">
       <div id="main">
-        <h3>lol</h3>
+        <h1>Frais capitalisés</h1>
+        <table id="FraisCapTable">
+          <thead>
+            <tr>
+              <th>Frais</th>
+              <th>Date du paiement</th>
+              <th>Table de référence</th>
+              <th>Taux d'intérêt de la capitalisation</th>
+              <th>Total (€)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {fraisRows.map((row, index) => (
+              <tr key={index}>
+                <td>
+                  <input type="text" defaultValue={row.frais} />
+                </td>
+                <td>
+                  <input type="date" defaultValue={row.date} />
+                </td>
+                <td>
+                  <select defaultValue={row.reference}>
+                    <option>Schryvers 2024 | VA rente viagère de 1 euro par an mensuel</option>
+                    <option>
+                      Schryvers 2024 | VA rente viagère de 1 euro par an mensuel (65 ans)
+                    </option>
+                    <option>
+                      Schryvers 2024 | VA rente viagère de 1 euro par an mensuel (66 ans)
+                    </option>
+                    <option>
+                      Schryvers 2024 | VA rente viagère de 1 euro par an mensuel (67 ans)
+                    </option>
+                    <option>
+                      Schryvers 2024 | VA rente viagère de 1 euro par an mensuel (68 ans)
+                    </option>
+                  </select>
+                </td>
+                <td>
+                  <select defaultValue={row.taux}>
+                    <option value="">Sélectionnez</option>
+                    <option>0.5</option>
+                    <option>0.8</option>
+                    <option>1</option>
+                    <option>1.5</option>
+                    <option>2</option>
+                    <option>3</option>
+                  </select>
+                </td>
+                <td>
+                  <input type="number" defaultValue={row.total} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
