6c1511f6e99f8dfad22f3f9c8bbb60cb3a250cbd
Thomas Point <thomas@makeit-studio.com>
2025-01-28 21:53:58 +0100

    copy date

--- src/renderer/src/form/incapacite_temp/economique.tsx
+++ src/renderer/src/form/incapacite_temp/economique.tsx
@@ -102,7 +102,7 @@ const ITEconomiqueForm = ({ initialValues, onSubmit, editable = true }) => {
   const copyDate = useCallback(
     (name, fieldName) => {
       const initial = typeof name === 'string' ? get(data, name) : name
-      let filteredData = initial.map(({ start, end }) => ({ start, end, amount: 30 }))
+      let filteredData = initial.map(({ start, end }) => ({ start, end }))
       const currentData = cloneDeep(formValues?.[fieldName])
       if (currentData) {
         filteredData = currentData.concat(filteredData)
