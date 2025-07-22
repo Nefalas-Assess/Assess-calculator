fbe2086bcb7620983211e7581793365caea3bfb7
Thomas Point <thomas@makeit-studio.com>
2025-07-22 10:17:27 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/renderer/src/components/incapacite_permanente/economique_cap.tsx
+++ src/renderer/src/components/incapacite_permanente/economique_cap.tsx
@@ -5,30 +5,30 @@ import { AppContext } from '@renderer/providers/AppProvider'
 import React, { useCallback, useContext, useRef } from 'react'
 
 const EconomiqueCap = ({ editable }) => {
-  const { data, setData } = useContext(AppContext)
+	const { data, setData } = useContext(AppContext);
 
-  const ref = useRef(null)
+	const ref = useRef(null);
 
-  const saveData = useCallback(
-    (values) => {
-      setData({ incapacite_perma_economique_cap: values })
-    },
-    [setData]
-  )
+	const saveData = useCallback(
+		(values) => {
+			setData({ incapacite_perma_economique_cap: values });
+		},
+		[setData],
+	);
 
-  return (
-    <div id="content">
-      <div id="main" ref={ref}>
-        <IPEcoCapForm
-          onSubmit={saveData}
-          editable={editable}
-          initialValues={data?.incapacite_perma_economique_cap}
-        />
-        <TotalBox label={'incapacite_perma.eco_cap.total'} documentRef={ref} />
-        <TotalBoxInterest documentRef={ref} />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div id="content">
+			<div id="main" ref={ref}>
+				<IPEcoCapForm
+					onSubmit={saveData}
+					editable={editable}
+					initialValues={data?.incapacite_perma_economique_cap}
+				/>
+				<TotalBox label={"incapacite_perma.eco_cap.total"} documentRef={ref} />
+				<TotalBoxInterest documentRef={ref} />
+			</div>
+		</div>
+	);
+};
 
-export default EconomiqueCap
+export default EconomiqueCap;
